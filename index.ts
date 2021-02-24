94eb3c108dc5028c514668551f0b6a9bf83eb61a
JH <473731589@qq.com>
2021-02-24 22:17:32 +0800

    匹配任意文件

--- index.ts
+++ index.ts
@@ -11,32 +11,19 @@ server.on('request',(request:IncomingMessage,response:ServerResponse)=>{
     const {method,url:path,headers} = request
     console.log(path)
     const {pathname,search} = url.parse(path)
-    switch (pathname){
-        case '/index.html':
-            response.setHeader('Content-Type','text/html;charset-utf-8')
-            fs.readFile(p.resolve(publicDir,'index.html'),(error,data)=>{
-                if(error) throw error
-                response.end(data.toString())
+            const filename = pathname.substr(1)
+            //response.setHeader('Content-Type','text/css ; charset=utf-8')
+            fs.readFile(p.resolve(publicDir,filename),(error,data)=>{
+                if(error){
+                    response.statusCode = 404
+                    response.end()
+                }else {
+                    response.end(data.toString())
+                }
+
             });
-            break;
-        case '/style.css':
-            response.setHeader('Content-Type','text/css ; charset=utf-8')
-            fs.readFile(p.resolve(publicDir,'style.css'),(error,data)=>{
-                if(error) throw error;
-                response.end(data.toString())
-            })
-            break;
-        case '/main.js':
-            response.setHeader('Content-Type','text/javascript ; charset=utf-8')
-            fs.readFile(p.resolve(publicDir,'main.js'),(error,data)=>{
-                if(error) throw error;
-                response.end(data.toString())
-            })
-            break;
-        default:
-            response.statusCode = 404;
-            response.end()
-    }
+
+
 
 
 })
