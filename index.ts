23ea49e5946ac4e5a009ef181ab329eb1ae7ee95
JH <473731589@qq.com>
2021-02-24 22:52:05 +0800

    处理不存在的文件  404

--- index.ts
+++ index.ts
@@ -11,14 +11,28 @@ server.on('request',(request:IncomingMessage,response:ServerResponse)=>{
     const {method,url:path,headers} = request
     console.log(path)
     const {pathname,search} = url.parse(path)
-            const filename = pathname.substr(1)
-            //response.setHeader('Content-Type','text/css ; charset=utf-8')
+            let filename = pathname.substr(1)
+            if(filename === ''){
+                filename = 'index.html'
+            }
+            //response.setHeader('Content-Type','text/html ; charset=utf-8')
             fs.readFile(p.resolve(publicDir,filename),(error,data)=>{
                 if(error){
-                    response.statusCode = 404
-                    response.end()
+                    if(error.errno === -4058){   //文件不存在
+                        response.statusCode = 404
+                        fs.readFile(p.resolve(publicDir,'404.html'),(error,data)=>{
+                            response.end(data)
+                        })
+                    }else if(error.errno === -4068){
+                        response.statusCode = 403
+                        response.end('无权查看内容')
+                    }else {
+                        response.statusCode = 500
+                        response.end('服务器繁忙')
+                    }
                 }else {
-                    response.end(data.toString())
+
+                    response.end(data)
                 }
 
             });
